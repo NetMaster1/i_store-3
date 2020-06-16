009d1702744eea57d7e541080b53016372b46414
Sergey Vinokurov <79200711112@yandex.ru>
2020-06-17 01:41:55 +0300

    First Commit with Postresql

--- pr_dir/settings.py
+++ pr_dir/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'pr_dir.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'i_store_3',
-#         'USER': 'postgres',
-#         'PASSWORD': 'ylhio65v',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'i_store_3',
+        'USER': 'postgres',
+        'PASSWORD': 'ylhio65v',
+        'HOST': 'localhost',
+        'PORT': '5433'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -141,7 +141,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pr_dir/static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 django_heroku.settings(locals())
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
