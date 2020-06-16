0b78c752b7b234d37bbbb884c19e0ed9cc42af7c
Sergey Vinokurov <79200711112@yandex.ru>
2020-06-17 02:54:42 +0300

    Postgresql 10

--- pr_dir/settings.py
+++ pr_dir/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'pr_dir.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'i_store_3',
         'USER': 'postgres',
         'PASSWORD': 'ylhio65v',
