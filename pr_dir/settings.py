9f90e49885a3b302c414d201e20a9ef0c8a9aad4
Sergey Vinokurov <79200711112@yandex.ru>
2020-06-24 20:52:28 +0300

    Entered Security Key

--- pr_dir/settings.py
+++ pr_dir/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '9=mm!b69r4d+*5-p)xd_47ei6-np82s(9ba4&#6z+bulph!ojv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
